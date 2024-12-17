2a5c8ab2fabd1372bb0c6ddda6e54dcd2bef54a8
Stephan Richter <s.richter@srsoftware.de>
2024-12-17 12:55:22 +0100

    fixed declaration in META-INF, published
    Signed-off-by: Stephan Richter <s.richter@srsoftware.de>

---
+++ de.srsoftware.tools.logging/src/test/java/de/srsoftware/tools/LoggingTest.java
@@ -0,0 +1,19 @@
+/* © SRSoftware 2024 */
+package de.srsoftware.tools;
+import static java.lang.System.Logger;
+import static java.lang.System.Logger.Level.*;
+
+import org.junit.jupiter.api.Test;
+
+public class LoggingTest {
+	private static final Logger LOG = System.getLogger(LoggingTest.class.getSimpleName());
+
+	@Test
+	public void doTest() {
+		LOG.log(ERROR, "This is an error message");
+		LOG.log(WARNING, "This is a warning");
+		LOG.log(INFO, "This is an informational message");
+		LOG.log(DEBUG, "You may debug this code");
+		LOG.log(TRACE, "Don`t you care about this!");
+	}
+}
