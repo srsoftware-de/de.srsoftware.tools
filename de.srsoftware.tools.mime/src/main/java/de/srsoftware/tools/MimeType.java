649613e002f70230c59dfd1e9c381187702a0936
Stephan Richter <s.richter@srsoftware.de>
2025-03-25 14:01:58 +0100

    added mime type 'application/x-www-form-urlencoded'
    Signed-off-by: Stephan Richter <s.richter@srsoftware.de>

--- de.srsoftware.tools.mime/src/main/java/de/srsoftware/tools/MimeType.java
+++ de.srsoftware.tools.mime/src/main/java/de/srsoftware/tools/MimeType.java
@@ -6,18 +6,31 @@ import java.io.IOException;
 import java.net.URL;
 
 public class MimeType {
-	public static final String MIME_DIA     = "image/dia";
-	public static final String MIME_GIF     = "image/gif";
-	public static final String MIME_HTML    = "text/html";
-	public static final String MIME_JPG     = "image/jpeg";
-	public static final String MIME_LATEX   = "text/x-tex";
-	public static final String MIME_PDF     = "image/pdf";
-	public static final String MIME_PNG     = "image/png";
-	public static final String MIME_SVG     = "image/svg";
-	public static final String MIME_TXT     = "text/plain";
-	public static final String MIME_UNKNOWN = "content/unknown";
-	public static final String MIME_XML     = "application/xml";
+	public static final String MIME_DIA      = "image/dia";
+	public static final String MIME_FORM_URL = "application/x-www-form-urlencoded";
+	public static final String MIME_GIF      = "image/gif";
+	public static final String MIME_HTML     = "text/html";
+	public static final String MIME_JPG      = "image/jpeg";
+	public static final String MIME_LATEX    = "text/x-tex";
+	public static final String MIME_PDF      = "image/pdf";
+	public static final String MIME_PNG      = "image/png";
+	public static final String MIME_SVG      = "image/svg";
+	public static final String MIME_TXT      = "text/plain";
+	public static final String MIME_UNKNOWN  = "content/unknown";
+	public static final String MIME_XML      = "application/xml";
 
+	/**
+	 * discourage entity creation
+	 */
+	private MimeType(){
+
+	}
+
+	/**
+	 * try to guess the mime type based on the url
+	 * @param url the url used to guess the mime type
+	 * @return the mime type string
+	 */
 	public static String guessMime(URL url) {
 		var parts     = url.toString().split("\\.");
 		var extension = parts[parts.length - 1].toLowerCase();
@@ -26,8 +39,13 @@ public class MimeType {
 				case "dia" -> MIME_DIA;
 				case "gif" -> MIME_GIF;
 				case "jpg", "jpeg" -> MIME_JPG;
+				case "htm", "html" -> MIME_HTML;
+				case "pdf" -> MIME_PDF;
 				case "png" -> MIME_PNG;
+				case "svg" -> MIME_SVG;
 				case "tex" -> MIME_LATEX;
+				case "txt" -> MIME_TXT;
+				case "xml" -> MIME_XML;
 				default -> url.openConnection().getContentType();
 			};
 		} catch (IOException e) {
@@ -35,6 +53,11 @@ public class MimeType {
 		}
 	}
 
+	/**
+	 * try to gues the mime type of a file
+	 * @param file the file for which the mime type is requested
+	 * @return the mime type string
+	 */
 	public static String guessMime(File file){
 		try {
 			return guessMime(file.toURI().toURL());
